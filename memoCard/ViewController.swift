46285e434904ea9cc6bd8ae522b02a2be030d7d2
liquorwell36 <liquorwell36@gmail.com>
2021-12-12 18:21:21 +0900

    Initial Commit

---
+++ memoCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memoCard
+//
+//  Created by kosuke sakai on 2021/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
